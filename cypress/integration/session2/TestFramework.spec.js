988b10de29d54f17cf11ab94b1687afcd88f939f
wenderson <wendrson22@gmail.com>
2022-04-05 21:53:05 -0300

    Finalizando cenários de teste

--- cypress/integration/session2/TestFramework.spec.js
+++ cypress/integration/session2/TestFramework.spec.js
@@ -7,38 +7,52 @@ describe('Framework', () => {
   beforeEach(function () {
     cy.fixture('example').then((data) => {
       this.data = data
-    })
-  })
+    });
+  });
+
   it('start test with building', function () {
     cy.visit('https://rahulshettyacademy.com/angularpractice/')
 
-    homePage.getEditBox().should('have.attr', 'minlength', '2')
-    homePage.getEditBox().type(this.data.name)
-    homePage.getTwoDataBinding().should('have.value', this.data.name)
-    homePage.getPassword().type(this.data.password, { log: false })
-    homePage.getGender().select(this.data.gender)
-    homePage.getEntrepreneaur().should('be.disabled')
-    homePage.getShopTab().click()
+    homePage.getEditBox().should('have.attr', 'minlength', '2');
+    homePage.getEditBox().type(this.data.name);
+    homePage.getTwoDataBinding().should('have.value', this.data.name);
+    homePage.getPassword().type(this.data.password, { log: false });
+    homePage.getGender().select(this.data.gender);
+    homePage.getEntrepreneaur().should('be.disabled');
+    homePage.getShopTab().click();
 
     this.data.productName.forEach((element) => {
-      cy.addProductCart(element)
-    })
+      cy.addProductCart(element);
+    });
 
     productPage.checkOutButton().click();
+
+    var sum = 0;
+
+    cy.get('tr td:nth-child(4) strong').each(($el) => {
+      const amount = $el.text();
+      var res = amount.split(" ");
+      res = res[1].trim();
+      sum = sum + parseInt(res);
+    }).then(() => {
+      cy.get('tr td:nth-child(5) strong').should('have.text', `₹. ${sum}`);  //sum is the total amount
+    })
+
     cy.contains("Checkout").click();
+
     cy.get('input#country').type(this.data.country);
+
     cy.get('.suggestions > ul > li > a').click();
+
     cy.get('#checkbox2').check({ force: true }).should('be.checked');
+
     cy.get('input[value="Purchase"]').click();
+
     //cy.get('div.alert-success').should('be.visible').should('contain', 'Success! Thank you!');
     cy.get('div.alert-success').then((text) => {
-      const actualText = text.text()
+      const actualText = text.text();
       expect(actualText.includes('Success')).to.be.true
-      cy.log('Test passed')
-    })
-  })
-
-  it('Add product in checkout', () => {
-    cy.visit('https://rahulshettyacademy.com/angularpractice/shop')
-  })
-})
+      cy.log('Test passed');
+    });
+  });
+});
